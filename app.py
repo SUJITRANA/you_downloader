22e5af5dbd94b5a16680d5bd61853a3e743413be
SUJITRANA <53816383+SUJITRANA@users.noreply.github.com>
2024-06-16 03:56:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,14 +14,6 @@ eventlet.monkey_patch()
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
 
-# AWS S3 configuration
-s3 = boto3.client(
-    's3',
-    aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
-    aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
-    region_name=os.getenv('AWS_REGION')
-)
-bucket_name = os.getenv('AWS_S3_BUCKET_NAME')
 
 # Use the user's Downloads directory
 home_directory = os.path.expanduser('~')
